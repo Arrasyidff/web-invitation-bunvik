f23c931e051e84154dc9bfcb4fcf52f260dfc61b
Arrasyid Fadel Fatonsyah <aff.anton20@gmail.com>
2026-05-14 20:09:09 +0700

    feat: Add cover flower images and update CoverSection component layout. Integrated left cover flower image and adjusted styling for improved visual presentation.

--- src/features/cover/components/CoverSection.tsx
+++ src/features/cover/components/CoverSection.tsx
@@ -1,9 +1,6 @@
-import {
-  GROOM_NAME_SHORT,
-  BRIDE_NAME_SHORT,
-  WEDDING_DATE_DISPLAY,
-  VENUE_NAME,
-} from "@/lib/constants";
+import Image from "next/image";
+import coverFlowerLeft from "@/app/assets/cover-flower-left.png";
+import coverFlowerRight from "@/app/assets/cover-flower-right.png";
 
 interface CoverSectionProps {
   onOpen: () => void;
@@ -13,9 +10,12 @@ export function CoverSection({ onOpen }: CoverSectionProps) {
   return (
     <section
       id="cover"
-      // className="min-h-screen flex flex-col items-center justify-center relative bg-[#fdf6ee] px-8 py-16 overflow-hidden"
+      className="min-h-screen flex flex-col items-center justify-space-between overflow-hidden bg-red-500"
     >
-      
+      <div className="bg-blue-500 w-full">
+        <Image src={coverFlowerLeft} alt="Cover" width={280} />
+      </div>
+      {/* <Image src={coverFlowerRight} alt="Cover" width={1000} height={1000} /> */}
     </section>
   );
 }
